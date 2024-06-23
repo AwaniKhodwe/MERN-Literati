1acea9ebab69ec6c57732bf24684d80c6850fffd
AwaniKhodwe <109502652+AwaniKhodwe@users.noreply.github.com>
2024-06-23 21:45:27 +0530

    Update SearchComp.js, removed React Reveal effect

--- src/components/SearchComp.js
+++ src/components/SearchComp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Element } from "react-scroll";
-import Fade from "react-reveal/Fade";
+
 import Libshelf from "../images/libraryshelf.png";
 import { useNavigate } from 'react-router-dom';
 
@@ -15,7 +15,7 @@ function SearchComp() {
   return (
     <Element name="fadeInDiv" className="relative h-96">
         {/* This is the div you want to animate */}
-        <Fade>
+
     <div className="w-full h-96" style={{ backgroundImage: `url(${Libshelf})`}}>
       <div className="w-1/3 text-center mx-auto pt-20">
           <h1 className="font-montserrat font-bold text-lavendargray text-5xl text-center">Search with Ease!</h1>
@@ -23,7 +23,6 @@ function SearchComp() {
           <button onClick={navigateToSearch} className="mt-8 w-48 h-12 bg-rusticred hover:bg-rusticred/75 text-orange-100 font-semibold font-montserrat py-2 px-4 rounded-full">Search</button>
       </div>
     </div>
-    </Fade>
     </Element>
     
 
